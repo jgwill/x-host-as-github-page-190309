f6d8624ab95332aefd7595a2e7a8bea3f394aa4f
Guillaume Isabelle <jgi@jgwill.com>
2019-03-19 13:05:20 -0400

    I love my new Resource Card :)

--- src/app/x/xlayoutingsimple/xlayoutingsimple.component.ts
+++ src/app/x/xlayoutingsimple/xlayoutingsimple.component.ts
@@ -12,14 +12,21 @@ export class XlayoutingsimpleComponent implements OnInit {
     goal: "Mastery Responsiveness with Angular/Layout/Flex",
     path: "src/app/x/xlayoutingsimple/xlayoutingsimple.component.html",
     state: "completed",
-    content:"The layout Stack itself when small device.",
-    resources:[{
-      icon:"perm_data_setting",
-      title:"Layouting on StackBlitz",
-      subtitle:"simple stack FlexLayout",
-      url:"https://stackblitz.com/edit/angular-flex-layout-seed-gyo38a?file=app%2Fapp.component.ts",
-      note:"I dont get what the media-query-status does, not really important"
-    }]
+    content: "The layout Stack itself when small device. basic html is : <div fxLayout=\"row\"  fxLayoutAlign=\"center start\"  fxLayout.xs=\"column\" fxFlexFill >",
+    resources: [{
+      icon: "perm_data_setting",
+      title: "Layouting on StackBlitz",
+      subtitle: "simple stack FlexLayout",
+      url: "https://stackblitz.com/edit/angular-flex-layout-seed-gyo38a?file=app%2Fapp.component.ts",
+      note: "I dont get what the media-query-status does, not really important"
+    }, {
+      icon: "format_align_center",
+      title: "Layout Demos - Alignments",
+      subtitle: "Flex showing live the different layout align",
+      url: "https://tburleson-layouts-demos.firebaseapp.com/#/docs",
+      note: "Really great visually insightful"
+    }
+    ]
   };
   constructor() { }
 
