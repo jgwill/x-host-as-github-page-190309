ccbdd450196539fd02dbc99401620359553b4fe3
Guillaume Isabelle <jgi@jgwill.com>
2019-03-19 11:52:03 -0400

    upgraded STC with variable object in the TS rathen than in the HTML

--- src/app/x/xflexlayout/xflexlayout.component.ts
+++ src/app/x/xflexlayout/xflexlayout.component.ts
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class XflexlayoutComponent implements OnInit {
 
+  stc = {
+    goal: "Mastery Responsiveness with Angular/Layout/Flex",
+    path: "src/app/x/xflexlayout/xflexlayout.component.html",
+    state: "completed",
+    content:"The layout Stack itself when small device."
+  };
+
   constructor() { }
 
   ngOnInit() {
